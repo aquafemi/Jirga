e6d12f0ddc5a50ed9e6fb206d80e2a2a030827e1
rushinjgr <rushinjgr@gmail.com>
2015-03-27 21:44:39 -0400

    good stopping point. not really

--- login.py
+++ login.py
@@ -2,7 +2,8 @@ import webapp2
 from webapp2_extras import sessions
 import os
 from google.appengine.ext.webapp import template
-import sessions_module;
+import sessions_module
+from model.User import User
 
 
 def render_template(handler, template_name, template_values):
@@ -14,20 +15,17 @@ class MainHandler(sessions_module.BaseSessionHandler):
 
     def get(self):
         print("yo")
-        if self.session.get('counter'):
-            self.response.out.write('Session is in place')
-            counter = self.session.get('counter')
-            self.session['counter'] = counter + 1
-            self.response.out.write('Counter = ' + str(self.session.get('counter')))
+        if self.session.get('user'):
+            self.response.out.write('User is already logged in')
         else:
-            self.response.out.write('Fresh Session')
-            self.session['counter'] = 1
-            self.response.out.write('Counter = ' + str(self.session.get('counter')))
+            self.response.out.write('User is not logged in')
     #post to login will receive:
     #username="username"
     #password="password"
     #should give user session
     def post(self):
-        print "word"
+        username=self.request.get('username')
+        password=self.request.get('username')
+        #uquery=User.all.
 
 app = webapp2.WSGIApplication([('/login', MainHandler)], config=sessions_module.myconfig_dict, debug=True)
