142f35d3180d1bf0559f38c50b31f5b2f65c3c83
rushinjgr <rushinjgr@gmail.com>
2015-03-28 12:44:41 -0400

    beginning viewQuestion

---
+++ viewQuestion.py
@@ -0,0 +1,33 @@
+from google.appengine.ext import db
+import webapp2
+from webapp2_extras import sessions
+import os
+from google.appengine.ext.webapp import template
+from model.Question import Question
+from model.Jirga import Jirga
+import sessions_module
+from model.User import User,Session
+import uuid
+
+def render_template(handler, template_name, template_values):
+    path = os.path.join(os.path.dirname(__file__), 'templates/' + template_name)
+    html = template.render(path, template_values)
+    handler.response.out.write(html)
+
+class MainHandler(sessions_module.BaseSessionHandler):
+
+    def get(self,questionId):
+        loggedIn= False
+        user = self.getuser()
+        if(user is not None):
+            question = Question.all().filter('qId', questionId).get()
+
+            render_template(self,"jirgaSettings.html",template_params)
+        else:
+            self.response.write("FAIL - you need to be logged in for this")
+
+    def post(self):
+
+
+
+app = webapp2.WSGIApplication([('/viewQuestion/(.*?)', MainHandler)], config=sessions_module.myconfig_dict, debug=True)
