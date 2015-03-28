450b9130dcb4bc38b910e86e882fc8204d25c59a
rushinjgr <rushinjgr@gmail.com>
2015-03-28 19:56:25 -0400

    kevin debug

--- getMyJirgas.py
+++ getMyJirgas.py
@@ -11,19 +11,22 @@ class MainHandler(sessions_module.BaseSessionHandler):
 
     def get(self,sessId):
         sess = Session.all().filter('sessId',sessId).get()
-        user = User.all().filter('username',sess.user)
-        if(user is not None):
-            jirgas = Jirga.get(user.jirgas)
-            result = {}
-            self.response.headers['Content-Type'] = 'application/json'
-            for jirga in jirgas:
-                obj2 = {
-                    'name': jirga.title,
-                    'key': jirga.jirgaId,
-                }
-                result.update(obj2)
-            self.response.out.write(json.dumps(result))
-        else:
-            self.response.write("FAIL - not logged in")
+        if sess is not None:
+            user = User.all().filter('username',sess.user).get()
+            print("user" + user.username)
+            if(user is not None):
+                jirgas = Jirga.get(user.jirgas)
+                result = {}
+                self.response.headers['Content-Type'] = 'application/json'
+                for jirga in jirgas:
+                    obj2 = {
+                        'name': jirga.title,
+                        'key': jirga.jirgaId,
+                    }
+                    result.update(obj2)
+                    print(jirga.title)
+                self.response.out.write(json.dumps(result))
+            else:
+                self.response.write("FAIL - not logged in")
 
 app = webapp2.WSGIApplication([('/getMyJirgas/(.*?)', MainHandler)], config=sessions_module.myconfig_dict, debug=True)
