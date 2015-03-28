c4f69372c77ff1d48a4208cb3c8e4a648f438425
rushinjgr <rushinjgr@gmail.com>
2015-03-28 02:42:53 -0400

    model tweaks

--- model/User.py
+++ model/User.py
@@ -4,3 +4,7 @@ class User(db.Model):
     username = db.StringProperty()
     password = db.StringProperty()
     jirgas = db.ListProperty(db.Key)
+
+class Session(db.Model):
+    sessId = db.StringProperty()
+    user = db.StringProperty()
