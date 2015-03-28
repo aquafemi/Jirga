ad19353ef04b08d3aa17d21ba3d7fe754318220f
rushinjgr <rushinjgr@gmail.com>
2015-03-27 23:51:50 -0400

    modified user for simplicity

--- model/User.py
+++ model/User.py
@@ -3,4 +3,4 @@ from google.appengine.ext import db
 class User(db.Model):
     username = db.StringProperty()
     password = db.StringProperty()
-    public_jirgas = db.ListProperty(db.Key)
+    jirgas = db.ListProperty(db.Key)
