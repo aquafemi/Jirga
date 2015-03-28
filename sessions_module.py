434d4710c0510a76f757b104bc9d4478b3a69498
rushinjgr <rushinjgr@gmail.com>
2015-03-28 02:41:24 -0400

    get user util method added

--- sessions_module.py
+++ sessions_module.py
@@ -5,6 +5,9 @@ import os
 
 #This is needed to configure the session secret key
 #Runs first in the whole application
+import model
+from model.User import Session, User
+
 myconfig_dict = {}
 myconfig_dict['webapp2_extras.sessions'] = {
     'secret_key': 'my-super-secret-key-somemorearbitarythingstosay',
@@ -27,4 +30,13 @@ class BaseSessionHandler(webapp2.RequestHandler):
     def session(self):
         # Returns a session using the default cookie key.
         return self.session_store.get_session()
+
+    def getuser(self):
+        if self.session.get('sessId'):
+            i = str(self.session.get('sessId'))
+            sess = Session.all().filter('sessId',i).get()
+            user = User.all().filter('username',sess.user).get()
+            return user
+        else:
+            return None
 #End of BaseSessionHandler Class
