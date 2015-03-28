f935ad16c8da7745c178e6e107aa5b609193e977
rushinjgr <rushinjgr@gmail.com>
2015-03-28 05:50:20 -0400

    getMyQuestions should be working

--- model/Question.py
+++ model/Question.py
@@ -3,4 +3,5 @@ from google.appengine.ext import db
 class Question(db.Model):
     questionString = db.StringProperty()
     votes = db.ListProperty(db.Key)
+    author = db.StringProperty()
     #comments = db.ListProperty(db.Key)
--- model/Vote.py
+++ model/Vote.py
@@ -2,4 +2,5 @@ from google.appengine.ext import db
 
 class Vote(db.Model):
     answer = db.StringProperty()
+    number = db.IntegerProperty()
     count = db.IntegerProperty()
