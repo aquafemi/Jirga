ee191be68eb3671e0c1e12dd49bbef608883d211
rushinjgr <rushinjgr@gmail.com>
2015-03-28 04:01:53 -0400

    getting api working

--- model/Jirga.py
+++ model/Jirga.py
@@ -3,6 +3,7 @@ from google.appengine.ext import db
 class Jirga(db.Model):
     title = db.StringProperty()
     members = db.ListProperty(db.Key)
-    owner = db.ListProperty(db.Key)
+    owner = db.StringProperty() #username of the owner
     questions = db.ListProperty(db.Key)
     publicJirga = db.IntegerProperty() #0 if private, 1 if public
+    jirgaId = db.StringProperty()
