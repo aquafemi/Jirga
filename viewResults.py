3c45f60667f24ac801d66708040b600abfe6626e
aquafemi <oolukoya@andrew.cmu.edu>
2015-03-28 23:08:16 -0400

    viewResults

---
+++ viewResults.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import db
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+import sessions_module
+from model.User import User,Session
+from model.Jirga import Jirga
+from model.Question import Question
+from model.Vote import Vote
+
+def render_template(handler, template_name, template_values):
+    path = os.path.join(os.path.dirname(__file__), 'templates/' + template_name)
+    html = template.render(path, template_values)
+    handler.response.out.write(html)
+
+class MainHandler(sessions_module.BaseSessionHandler):
+
+    def get(self, qId):
+        user = self.getUser()
+        if(user is not None):
+            question = Question.all().filter('qId',qId).get()
+            votes = Vote.get(question.votes)
+            template_params = {
+                'question':question,
+                'votes':votes
+            }
+            render_template(self,"viewResults.html",template_params)
+        else:
+            self.response.write("FAIL - you need to be logged in for this")
+
+app = webapp2.WSGIApplication([('/viewResults(.*?)', MainHandler)], config=sessions_module.myconfig_dict, debug=True)
